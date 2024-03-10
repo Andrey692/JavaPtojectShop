efdb2f2df40c0f888aac576087acfcf0ae7563bd
Andrey <bot6923777@outlook.com>
2024-03-10 18:07:39 +0300

    добавил решение

--- Laptop.java
+++ Laptop.java
@@ -18,47 +18,6 @@ public class Laptop {
         this.color = color;
     }
 
-    // private String getName() {
-    // return name;
-    // }
-
-    // private int getRAM() {
-    // return RAM;
-    // }
-
-    // private int getHDD() {
-    // return HDD;
-    // }
-
-    // private String getOS() {
-    // return OS;
-    // }
-
-    // private String getColor() {
-    // return color;
-    // }
-
-    // private void setName(String name) {
-    // this.name = name;
-    // }
-
-    // private void setRAM(int RAM) {
-    // this.RAM = RAM;
-
-    // }
-
-    // private void setHDD(int HDD) {
-    // this.HDD = HDD;
-    // }
-
-    // private void setOS(String OS) {
-    // this.OS = OS;
-    // }
-
-    // private void setColor(String color) {
-    // this.color = color;
-    // }
-
     // переопределяем метод hashCode (только для ссылочных типов данных)
     @Override
     public int hashCode() {
@@ -78,7 +37,7 @@ public class Laptop {
         // и добавляем логические выражения сравнения пяти полей, которые должны быть
         // равны:
         return name.equals(lap.name) && // тип String сравниваем через equals
-                RAM == lap.RAM && // сравниваем значения RAM (тип int)
+                RAM == lap.RAM &&
                 HDD == lap.HDD && // сравниваем значения RAM (тип int)
                 OS.equals((lap.OS)) && // тип String сравниваем через equals
 
@@ -88,11 +47,19 @@ public class Laptop {
     // переопределяем метод
     @Override
     public String toString() {
-        return "Название : " + name + ", " +
-                "RAM: " + RAM + ", " +
-                "HDD: " + HDD + ", " +
-                "ОС: " + OS + ", " +
-                "Цвет: " + color;
+        return "(Название: " + name + "), " +
+                "(RAM:" + RAM + "), " +
+                "(HDD:" + HDD + "), " +
+                "(ОС: " + OS + "), " +
+                "(Цвет: " + color + ")";
+    }
+
+    int getRAM() {
+        return this.RAM;
+    }
+
+    int getHDD() {
+        return this.HDD;
     }
 
 }
--- mainLaptop.java
+++ mainLaptop.java
@@ -1,7 +1,21 @@
+// -Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в
+// java.
+// -Создать множество ноутбуков.
+// -Написать метод, который будет запрашивать у пользователя критерий (или критерии) фильтрации и
+// выведет ноутбуки, отвечающие фильтру. Критерии фильтрации можно хранить в Map. Например:
+// “Введите цифру, соответствующую необходимому критерию: 1 - ОЗУ
+// 2 - Объем ЖД
+// 3 - Операционная система
+// 4 - Цвет …
+// -Далее нужно запросить минимальные значения для указанных критериев - сохранить параметры фильтрации
+// можно также в Map.
+// -Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
+
 package Shop;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class mainLaptop {
@@ -17,19 +31,39 @@ public class mainLaptop {
         Laptop L09 = new Laptop("HP", 12, 256, "Windows10", "blue");
         Laptop L10 = new Laptop("Huawei", 12, 512, "Windows11", "black");
         Laptop L11 = new Laptop("HP", 16, 512, "Windows10", "grey");
-        Laptop L12 = new Laptop("Aser", 16, 512, "Windows11", "black");
+        Laptop L12 = new Laptop("Aser", 32, 1024, "Windows11", "black");
         Laptop L13 = new Laptop("Asus", 16, 512, "Windows10", "grey");
         Laptop L14 = new Laptop("HP", 16, 512, "Windows11", "blue");
         Laptop L15 = new Laptop("HP", 16, 512, "Windows11", "blue");
 
+        // cоздаем множество и добавляем в него все ноутбуки
         Set<Laptop> laptops = new HashSet<>(
                 Arrays.asList(L01, L02, L03, L04, L05, L06, L07, L08, L09, L10, L11, L12, L13, L14, L15));
-
         for (Laptop el : laptops) { // перебираем коллекцию laptops
             System.out.println(el);
+        }
+        System.out.printf("Колличество уникальных ноутбуков: %d \n", laptops.size());
 
+        // Считываем с консоли параметры для фильтрации
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Выбор ноутбука по двум параметрам: 1. объем оперативной памяти: ");
+        int RAMuser = sc.nextInt();
+
+        System.out.println("2. объем жеского диска: ");
+        int HDDuser = sc.nextInt();
+
+        // Проверка на корректность ввода
+        if ((RAMuser >= 33) || (HDDuser >= 1024)) {
+            System.out.println("Некорректный ввод!!!");
         }
 
+        // Фильтрация ноутбуков и вывод в консоль
+        for (Laptop el : laptops) {
+            if ((el.getRAM() >= RAMuser) && (el.getHDD() >= HDDuser)) {
+                System.out.println(el.toString());
+            }
+        }
+        sc.close();
     }
 
 }
